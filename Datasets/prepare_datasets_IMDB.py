f0e4ba2cc56d381c4abc9faf496f89dcdacf2be6
omrimaoz <omrimaoz@gmail.com>
2022-02-20 21:13:00 +0200

    First commit from LSTM_add: Updates with IMDB dataset Create the LSTM model -> Model.py, Dataset.py, Encode_Dataset.py, Main.py

--- Datasets/prepare_datasets_IMDB.py
+++ Datasets/prepare_datasets_IMDB.py
@@ -1,10 +1,14 @@
+import os
+import re
+
 import numpy as np
 import pandas as pd
 import json
 import stanza
 from zipfile import ZipFile
 
-limit = 3000
+limit = 25000
+folder = './Datasets/'
 
 dict_to_json = dict()
 df = pd.read_csv("Datasets/IMDB Dataset_v1.csv")
@@ -16,7 +20,19 @@ UPOS documentation - https://universaldependencies.org/u/pos/
 stanza.download('en')
 nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos')
 
+listdir = os.listdir(folder)
+regex = re.compile('_(\d+)\.json')
+listdir = [regex.findall(file_name) for file_name in listdir if 'IMDB' in file_name]
+listdir = [int(file_name[0]) for file_name in listdir if len(file_name) > 0]
+listdir.sort()
+if len(listdir) > 0:
+    with open(folder + 'IMDB_Dataset_{}.json'.format(listdir[-1]), 'r') as f:
+        dict_to_json = json.loads(f.read())
+    df = df[listdir[-1]:]
+
 for i, row in df.iterrows():
+    if (i+1) % 1000 == 0:
+        print('Process {}/{}'.format(i+1, limit))
     dict_to_json[i] = {
         'review': row['review'],
         'sentiment': row['sentiment']
@@ -24,11 +40,14 @@ for i, row in df.iterrows():
     doc = nlp(row['review'])
     dict_to_json[i].update({"upos": " ".join([word.upos for sent in doc.sentences for word in sent.words])})
     dict_to_json[i].update({"xpos": " ".join([word.xpos for sent in doc.sentences for word in sent.words])})
-    if i >= limit:
-        break
 
-with open('IMDB_Dataset.json', 'w') as f:
-    json.dump(dict_to_json, f)
+    if (i+1) % 5000 == 0 or (i+1) >= limit:
+        file_name = folder + 'IMDB_Dataset_{}.json'.format(i+1)
+        with open(file_name, 'w') as f:
+            json.dump(dict_to_json, f)
+
+    if (i+1) >= limit:
+        break
 
 # with ZipFile('Datasets/IMDB_Dataset.zip', 'w') as zipObj:
 #     zipObj.write('Datasets/IMDB_Dataset.json')
---
+++ LSTM/Main.py
@@ -0,0 +1,89 @@
+import json
+import sys
+import time
+from collections import Counter
+
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+from sklearn.metrics import mean_squared_error
+from torch.utils.data import DataLoader
+
+from LSTM.Dataset import ReviewsDataset
+from sklearn.model_selection import train_test_split
+
+from LSTM.Model import LSTM
+
+dataset = sys.argv[1]
+tagging = sys.argv[2]  # tagging = 'review' / 'upos' / 'xpos'
+batch_size = 32
+limit = -1
+
+def train_model(model, criterion, train_loader, valid_loader, epochs=10, lr=0.01):
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = torch.optim.Adam(parameters, lr=lr)
+    for epoch in range(epochs):
+        start = time.time()
+        model.train()
+        sum_loss = 0.0
+        total = 0
+        for batch, pack in enumerate(train_loader):
+            x, y, l = pack
+            x = x.long()
+            y = y.long()
+            y_pred = model(x, l)
+
+            loss = criterion(y_pred, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            sum_loss += loss.item()*y.shape[0]
+            total += y.shape[0]
+            # if batch % 10:
+            #     print({'epoch': epoch, 'batch': batch})
+        val_loss, val_acc = validation_metrics(model, criterion, valid_loader)
+        end = time.time()
+        print('epoch: {epoch}, train_loss: {train_loss}, val_loss: {val_loss}, val_acc: {val_acc}, time: {time}s'.format(
+            epoch=epoch, train_loss=np.round(sum_loss/total, 3), val_loss=np.round(val_loss, 3),
+            val_acc=np.round(val_acc, 3), time=int(end-start)
+        ))
+
+def validation_metrics(model, criterion, valid_loader):
+    model.eval()
+    correct = 0
+    total = 0
+    sum_loss = 0.0
+    for x, y, l in valid_loader:
+        x = x.long()
+        y = y.long()
+        y_hat = model(x, l)
+        loss = criterion(y_hat, y)
+        pred = torch.max(y_hat, 1)[1]
+        correct += (pred == y).float().sum()
+        total += y.shape[0]
+        sum_loss += loss.item()*y.shape[0]
+    return sum_loss/total, correct/total
+
+# loading the data
+with open('../LSTM/Processed_' + tagging + '_' + dataset, 'r') as f:
+    reviews = json.loads(f.read())
+vocab_size = reviews['prop']['num_words']
+
+# check how balanced the dataset is
+print('How balanced the dataset is:')
+print(Counter([review['sentiment'] for _, review in reviews['data'].items()]))
+
+X = [(np.array(review['encode_sen']), review['len_sen']) for _, review in reviews['data'].items()][:limit]
+y = [review['sentiment'] for _, review in reviews['data'].items()][:limit]
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2)
+
+train_ds = ReviewsDataset(X_train, y_train)
+valid_ds = ReviewsDataset(X_valid, y_valid)
+train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+val_dl = DataLoader(valid_ds, batch_size=batch_size)
+
+model = LSTM(vocab_size, embedding_dim=30, hidden_dim=30)
+
+train_model(model, criterion=nn.CrossEntropyLoss(), train_loader=train_dl, valid_loader=val_dl, epochs=30)
